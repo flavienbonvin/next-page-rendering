bdc38ae47b7de956be871ee6817ed5cd00a368e5
Flavien Bonvin <flavien@coteries.com>
2022-08-11 23:09:22 +0200

    Add Plausible analytics

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
-const nextConfig = {
+const { withPlausibleProxy } = require("next-plausible")
+
+const nextConfig = withPlausibleProxy()({
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["raw.githubusercontent.com"],
   },
-}
+})
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
+import PlausibleProvider from "next-plausible"
 import type { AppProps } from "next/app"
 import Nav from "../components/tailwind/Nav"
 import "../styles/global.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <PlausibleProvider domain="next-speed.flavienbonvin.com">
       <Nav />
       <Component {...pageProps} />
-    </>
+    </PlausibleProvider>
   )
 }
 
